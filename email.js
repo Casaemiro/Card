6ed24caf46cc9f28c29759b732a2181c5ef817b9
Akoneh Silas <silasmagho18@gmail.com>
2022-05-19 15:31:46 +0100

    conbined-Single-File

--- email.js
+++ email.js
@@ -7,20 +7,32 @@ function toggleColor()
     let theme = document.getElementById("wbody")
     let cardtheme = document.getElementById("card")
     let contactTheme = document.getElementById("clickable")
-    let text = document.getElementsByTagName("a")
-    theme.style.backgroundColor = "White";
+    let text1 = document.getElementById("wer1")
+    let text2 = document.getElementById("wer2")
+    let text3 = document.getElementById("wer3")
+    let text4 = document.getElementById("wer4")
+    theme.style.backgroundColor = "white";
     cardtheme.style.backgroundColor = "White";
     contactTheme.style.color = "#333";
-    text.style.color = "#333"
+    text1.style.color = "#333"
+    text2.style.color = "#333"
+    text3.style.color = "#333"
+    text4.style.color = "#333"
   }else{
     let theme = document.getElementById("wbody")
     let cardtheme = document.getElementById("card")
-    let text = document.getElementsByTagName("a")
+    let text1 = document.getElementById("wer1")
+    let text2 = document.getElementById("wer2")
+    let text3 = document.getElementById("wer3")
+    let text4 = document.getElementById("wer4")
     let contactTheme = document.getElementById("clickable")
     theme.style.backgroundColor = "#333"
     cardtheme.style.backgroundColor = "#333";
     contactTheme.style.color = "White";
-    text.style.color = "white";
+    text1.style.color = "white"
+    text2.style.color = "white"
+    text3.style.color = "white"
+    text4.style.color = "white"
   }
   console.log("wow")
 }
